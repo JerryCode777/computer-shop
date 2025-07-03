4b5e3b43f378c93918dc4d793a2ffc9aee7e61ec
Jaime Igreda <jigreda@unsa.edu.pe>
2025-07-02 20:19:52 -0500

    convirtiendo el componente footer en standalone con CommonModule y RouterLink

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterLink]
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
