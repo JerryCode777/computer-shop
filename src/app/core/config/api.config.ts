a13c95e7f8d239204954a9eadb401e78d1627bac
Jaime Igreda <jigreda@unsa.edu.pe>
2025-07-03 18:59:33 -0500

    Agrando el archivo api.config con los endpoints organizados y parametrizados

---
+++ src/app/core/config/api.config.ts
@@ -0,0 +1,44 @@
+export const API_CONFIG = {
+  BASE_URL:'http://localhost:8000/api',
+  AUTH_TOKEN_KEY: 'auth_token',
+  ENDPOINTS: {
+    AUTH: {
+      LOGIN: '/usuarios/login/',
+      REGISTER: '/usuarios/register/',
+    },
+    USER: {
+      PROFILE: '/usuarios/user-detail/',
+      EDIT_PROFILE: '/usuarios/editProfile/',
+      ADDRESS: {
+        LIST: '/user/address/',
+        ADD: '/user/address/add/',
+        DELETE: (addressId: number) => `/user/address/delete/${addressId}`,
+        EDIT: (addressId: number) => `/user/address/edit/${addressId}`,
+      }
+    },
+    CART: {
+      GET: '/user/cart',
+      ADD_ITEM: '/user/cart/add',
+      REMOVE_ITEM: (cartItemId: number) => `/user/cart/item/remove/${cartItemId}`,
+      UPDATE_QUANTITY: (cartItemId: number, quantity: number) => `/user/cart/item/${cartItemId}/${quantity}`,
+      CLEAR: '/user/cart/clear'
+    },
+    PRODUCTS: {
+      LIST: '/products',
+      DETAILS: (id: number) => `/products/${id}`,
+      RATINGS: {
+        GET: (productId: number) => `/product/ratings/${productId}`,
+        CREATE: '/product/ratings/create'
+      },
+      REVIEWS: {
+        GET: (productId: number) => `/product/reviews/${productId}`,
+        CREATE: '/product/reviews/create'
+      }
+    },
+    ORDER: {
+      CREATE: '/user/order/create',
+      LIST: '/user/order/allOrders',
+      DETAILS: (orderId: number) => `/user/order/${orderId}`
+    }
+  }
+}; 
