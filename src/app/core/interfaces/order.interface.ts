90ce3583e35d0d4f9ab7b63186d7bbaa2fbcc171
Jaime Igreda <jigreda@unsa.edu.pe>
2025-07-04 10:12:52 -0500

    Implementando la interfaz order para las ordenes e items

--- src/app/core/interfaces/order.interface.ts
+++ src/app/core/interfaces/order.interface.ts
@@ -1,1 +1,47 @@
+import { Address } from "./address.interface";
+import { SkuProductDto } from "./cart.types";
 
+export interface OrderDto {
+    id: number;
+    order_id: string;
+    user_id: number;
+    orderItemList: OrderItemDTO[];
+    orderDate: string;
+    deliveryDate: string;
+    shippingAddress: Address;
+    totalPrice: number;
+    totalDiscountedPrice: number;
+    discount: number;
+    orderStatus: string;
+    totalItem: number;
+    createdAt: string;
+  }
+  
+ export interface OrderItemDTO {
+    id: number;
+    sku: OrderSkuDto;
+    quantity: number;
+    price: number;
+    discountedPrice: number;
+  }
+
+  export interface OrderHistoryResponse {
+    respType: string;
+    status: {
+      statusCode: number;
+      statusMessage: string;
+      statusMessageKey: string;
+    };
+    order_history: OrderDto[];
+  }
+
+  export interface OrderSkuDto{
+   id:number;
+   skuCode: string;
+   color: string;
+   price: number;
+   size: string;
+   discountedPrice: number;
+   discountPercent: number;
+   product:SkuProductDto;
+  }
