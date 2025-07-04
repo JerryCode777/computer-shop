dc1dfee55802f1b7fd71cb9b52e31f138cd380b2
Jaime Igreda <jigreda@unsa.edu.pe>
2025-07-03 21:29:32 -0500

    Implementando el servicio checkout para el manejo de direcciones y ordenes con autenticacion

--- src/app/core/services/checkout.service.ts
+++ src/app/core/services/checkout.service.ts
@@ -1,9 +1,66 @@
-import { Injectable } from '@angular/core';
+// services/checkout.service.ts
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Address } from '../interfaces/address.interface';
+import { API_CONFIG } from '../config/api.config';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+  private http = inject(HttpClient);
+  constructor(protected AuthService: AuthService){
 
-  constructor() { }
+  }
+  private getHeaders(): HttpHeaders {
+    const token = this.AuthService.getToken();
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
+  getAllAddresses(): Observable<any> {
+    return this.http.get(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.USER.ADDRESS.LIST}`,
+      { headers: this.getHeaders() }
+    );
+  }
+
+  addNewAddress(address: Address): Observable<any> {
+    return this.http.post(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.USER.ADDRESS.ADD}`,
+      address,
+      { headers: this.getHeaders() }
+    );
+  }
+
+  editAddress(addressId: number, address: Address): Observable<any> {
+    return this.http.put(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.USER.ADDRESS.EDIT(addressId)}`,
+      address,
+      { headers: this.getHeaders() }
+    );
+  }
+
+  deleteAddress(addressId: number): Observable<any> {
+    return this.http.delete(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.USER.ADDRESS.DELETE(addressId)}`,
+      { headers: this.getHeaders() }
+    );
+  }
+
+  createOrder(address: Address): Observable<any> {
+    return this.http.post(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.ORDER.CREATE}`,
+      address,
+      { headers: this.getHeaders() }
+    );
+  }
+
+  getCart(): Observable<any> {
+    return this.http.get(
+      `${API_CONFIG.BASE_URL}${API_CONFIG.ENDPOINTS.CART.GET}`,
+      { headers: this.getHeaders() }
+    );
+  }
 }
